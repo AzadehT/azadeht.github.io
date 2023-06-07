f73ebee1e5743d0d947ee675772221b6cfd9083b
A <azadeh.tabiban@gmail.com>
2023-06-06 23:25:47 -0400

    Add files via upload

---
+++ img/test.js
@@ -0,0 +1,9 @@
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NH4P0DK5FN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NH4P0DK5FN');
+</script>
